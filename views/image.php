adec05eab093c8c8cc9ab44ac37954bce7acc212
Thomas Edvalson <machin3@gmail.com>
2012-09-11 03:49:14 -0400

    Separate filename header for thumbnails

--- views/image.php
+++ views/image.php
@@ -21,10 +21,13 @@ try {
 
 $filename = $img->getFileName();
 
-if ($split_url[count($split_url)-2] == 't')
+if ($split_url[count($split_url)-2] == 't'){
 	$file = new fFile(CI_UPLOAD_DIR.'thumb/'.$filename);
-else
+	header('Content-Disposition: inline; filename="'.$filename.'_thumb"');
+} else {
 	$file = new fFile(CI_UPLOAD_DIR.$filename);
+	header('Content-Disposition: inline; filename="'.$filename.'"');
+}
 
 $lastModified = $file->getMTime()->format('D, d M Y H:i:s');
 header('Pragma: public');
@@ -32,11 +35,10 @@ header('Cache-Control: max-age='.CI_IMAGE_EXPIRATION);
 header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + CI_IMAGE_EXPIRATION));
 header('Content-Length: ' . $file->getSize());
 header('Content-Type: ' . $file->getMimeType());
-header('Content-Disposition: inline; filename="'.$filename.'"');
 if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $lastModified){
 	// Issue HTTP 304 for unchanged images to speed up the response
-	header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'], true, 304);
+	header('Last-Modified: '.$lastModified, true, 304);
 	die();
 }
-header('Last-Modified: ' . $lastModified);
+header('Last-Modified: '.$lastModified);
 $file->output(FALSE);
