b4e3c82ab1902cfa66bb074c7a6ce142608795f6
Cruel <machin3@gmail.com>
2013-03-04 22:14:20 -0500

    Added page navigation to admin image page

--- views/admin/images.php
+++ views/admin/images.php
@@ -4,8 +4,21 @@ $page = fRequest::get('page', 'integer', 1);
 $time = fRequest::get('time', 'integer', time());
 
 $images = fRecordSet::build('Image', array('time<'=>$time), array('time'=>'desc'), 25, $page);
+$count = $images->count(TRUE);
 
 ?>
+
+<nav id="admin-gallery-nav">
+	<?php
+		for ($i = 1; $i <= ceil($count/25); $i++) {
+			if ($i == $page) {
+				echo $i;
+			} else {
+				echo '<a href="'.URL_ROOT."admin/images?page=$i\">$i</a>";
+			}
+		}
+	?>
+</nav>
 
 <div class="admin-gallery">
 	<?php
