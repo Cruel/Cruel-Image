b04d83f8af663dd0ac30efc259ceb15c9994fa6d
Thomas Edvalson <machin3@gmail.com>
2013-10-15 13:55:46 -0400

    Load Flourish lib using Composer instead of git modules

--- inc/loader.php
+++ inc/loader.php
@@ -3,13 +3,11 @@
 define('DOC_ROOT', realpath(dirname(__FILE__) . '/../'));
 define('URL_ROOT', substr(DOC_ROOT, strlen(realpath($_SERVER['DOCUMENT_ROOT']))) . '/');
 
-// http://php.net/manual/en/language.oop5.autoload.php
-function __autoload($class) {
-	$file = DOC_ROOT . "/inc/flourish/$class.php";
-	if (file_exists($file))
-		return require $file;
+require(DOC_ROOT."/vendor/autoload.php");
+
+spl_autoload_register(function ($class) {
 	$file = DOC_ROOT . "/models/$class.php";
 	if (file_exists($file))
 		return require $file;
 	throw new Exception('The class ' . $class_name . ' could not be loaded');
-}
+});
