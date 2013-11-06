5a09467968829ab4886e12adeb4b00447d3126b9
Cruel <machin3@gmail.com>
2013-11-06 00:16:29 -0500

    Mah intellectual property!!

--- views/home/footer.php
+++ views/home/footer.php
@@ -8,7 +8,7 @@
 				<li><a href="/tos">Terms of Service</a></li>
 				<li><a href="/contact">Contact</a></li>
 			</ul>
-			<p>&copy; 2012 <a href="http://images.cruels.net/" target="_blank" title="Cruel Image Upload Script" id="foot-site-title">Cruel Image</a></p>
+			<p>&copy; 2012-<?php echo date("Y") ?> <a href="http://images.cruels.net/" target="_blank" title="Cruel Image Upload Script" id="foot-site-title">Cruel Image</a></p>
 		</div>
 		<a href="https://github.com/Cruel/Cruel-Image" target="_blank" id="powered" title="Cruel Image Hosting Script">Powered by <img src="<?php echo URL_ROOT.'static/themes/'.CI_THEME.'/img/powered.png' ?>" alt="Cruel Image hosting script" /></a>
 	</div>
